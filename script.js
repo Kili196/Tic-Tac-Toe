33ded7b09d2849108d86f57d4858080faf26335f
kili196 <kilian.baier16@gmail.com>
2024-07-30 17:50:29 +0200

    deleted useless if conditions

--- script.js
+++ script.js
@@ -53,39 +53,48 @@ const checkWin = (game_board) => {
         const firstCell = game_board[i][0];
         const secondCell = game_board[i][1];
         const thirdCell = game_board[i][2];
-        if(firstCell == secondCell && secondCell == thirdCell){
+    
+            if(firstCell == secondCell && secondCell == thirdCell && (firstCell != " " && secondCell != " " && thirdCell != " ")){
             isWin = true;
-        }
+            }   
+        
+        
     
     }
 
     //check columns
-    console.log(game_board[0][0]);
+    console.log(game_board[0][0] == "");
     console.log(game_board[1][0]);
     console.log(game_board[2][0]);
 
 
-    if(game_board[0][0] == game_board[1][0] &&  game_board[1][0] === game_board[2][0]){
-        isWin = true;
+       
+        if(game_board[0][0] == game_board[1][0] &&  game_board[1][0] === game_board[2][0] ){
+            isWin = true;
     }
-    else if(game_board[0][1] == game_board[1][1] &&  game_board[1][1] === game_board[2][1]){
-        isWin = true;
-    }
-    else if(game_board[0][2] == game_board[1][2] &&  game_board[1][2] === game_board[2][2]){
-        isWin = true;
-    }
+        else if(game_board[0][1] == game_board[1][1] &&  game_board[1][1] === game_board[2][1]){
+            isWin = true;
+        }
+        else if(game_board[0][2] == game_board[1][2] &&  game_board[1][2] === game_board[2][2]){
+            isWin = true;
+        }
+    
+   
 
 
     //diagonal from left to right
-
-    if(game_board[0][0] == game_board[1][1] && game_board[1][1] == game_board[2][2]){
-        isWin = true;
-    }
+    
+        console.log("hii")
+        if(game_board[0][0] == game_board[1][1] && game_board[1][1] == game_board[2][2]){
+            isWin = true;
+        }
 
     //diagonal from right to left
-    else if (game_board[0][2] == game_board[1][1] && game_board[1][1] == game_board[2][0]){
-        isWin = true;
-    }
+        else if (game_board[0][2] == game_board[1][1] && game_board[1][1] == game_board[2][0]){
+            isWin = true;
+        }
+ 
+
 
     if(isWin == true){
         console.log("There is a winner")
