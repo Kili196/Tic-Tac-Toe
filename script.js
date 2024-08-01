a81916ba64a519ee19c415d3e6f515c1d325560c
kili196 <kilian.baier16@gmail.com>
2024-08-01 13:26:41 +0200

    claned up code

--- script.js
+++ script.js
@@ -27,16 +27,20 @@ const getDomElementsAndVariables = () => {
     const currPlayer = player_1;
     const win_screen = document.getElementsByClassName("win-screen")
     const settings_button = document.getElementById("settings");
-    const dialog = document.getElementsByClassName("settings_dialog");
+    const dialog = document.getElementsByClassName("settings_dialog")[0];
     const clear = document.getElementById("clear");
-    return ({clear: clear, cells: cells, currPlayer: currPlayer, player_1, player_2, columns: columns, win_screen: win_screen, settings_button: settings_button, dialog: dialog})
-}
-
-const getPlayerNames = () => {
     const player_1_input = document.getElementsByClassName("player-1-class")[0];
     const player_2_input = document.getElementsByClassName("player-2-class")[0];
     const accept_button = document.getElementsByClassName("save-form")[0];
-    const dialog = document.getElementsByClassName("settings_dialog")[0];
+    return ({player_1_input: player_1_input, player_2_input: player_2_input, accept_button: accept_button, clear: clear, cells: cells, currPlayer: currPlayer, player_1, player_2, columns: columns, win_screen: win_screen, settings_button: settings_button, dialog: dialog})
+}
+
+const getPlayerNames = () => {
+    const domElements = getDomElementsAndVariables();
+    const player_1_input = domElements.player_1_input;
+    const player_2_input = domElements.player_2_input;
+    const accept_button = domElements.accept_button;
+    const dialog = domElements.dialog;
     
     accept_button.addEventListener("click", (e) => {
          player_1 = newPlayer(player_1_input.value, "X");
@@ -58,13 +62,9 @@ const addCellEventListener = () => {
                     game_board[gameboard_position[0]][gameboard_position[1]] = domElements.currPlayer.type;
                     console.log(domElements.currPlayer)
                     const res = checkWin(game_board, domElements.currPlayer);
-
                     checkDraw++;
-
                     if(res != null){
-                    
                         setWinScreen(domElements.win_screen[0], domElements.currPlayer)
-                       
                     }
                     domElements.currPlayer = domElements.currPlayer == player_1 ? player_2 : player_1;
 
@@ -82,7 +82,6 @@ const settingsDialog = () => {
     getPlayerNames();
 }
 
-
 const init_game = () => {
     settingsDialog();
     addCellEventListener();
@@ -90,15 +89,10 @@ const init_game = () => {
         clearGame();
     })
     getDomElementsAndVariables().win_screen.innerHTML = ""
-  
-    
 }
-
 const setWinScreen = (win_screen, player) => {
     win_screen.innerHTML = `${player.name} won!`;
 }
-
-
 
 const checkWin = (game_board, currPlayer) => {
     let isWin = false;
@@ -117,7 +111,6 @@ const checkWin = (game_board, currPlayer) => {
         }   
     
     }
-
     //check columns       
         if(game_board[0][0] === game_board[1][0] &&  game_board[1][0] === game_board[2][0] ){
             isWin = true;
@@ -130,13 +123,10 @@ const checkWin = (game_board, currPlayer) => {
         }
     
     //diagonal from left to right
-    
-      
         if(game_board[0][0] == game_board[1][1] && game_board[1][1] == game_board[2][2]){
          
             isWin = true;
         }
-
     //diagonal from right to left
         else if (game_board[0][2] == game_board[1][1] && game_board[1][1] == game_board[2][0]){
           
